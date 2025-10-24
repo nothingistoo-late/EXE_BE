a8623ba352a102c8e81082983615ff6c30a68aa9
hctrung2k4 <hctrung2k4@gmail.com>
2025-10-24 17:58:06 +0700

    Add migration for previous changes

---
+++ Repositories/Migrations/20251024105734_addFieldMiss.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFieldMiss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryTo",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWeeklyPackage",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledDeliveryDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WeeklyPackageId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryTo",
+                table: "GiftBoxOrders",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "GiftBoxOrders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryTo",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "IsWeeklyPackage",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledDeliveryDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "WeeklyPackageId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryTo",
+                table: "GiftBoxOrders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "GiftBoxOrders");
+        }
+    }
+}
