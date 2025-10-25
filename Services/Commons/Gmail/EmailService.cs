d2069573ce52e2df7461d83ca9e146d19e75edc4
hctrung2k4 <hctrung2k4@gmail.com>
2025-10-26 00:12:47 +0700

    add delay for gmail avoid too many request in resend

--- Services/Commons/Gmail/EmailService.cs
+++ Services/Commons/Gmail/EmailService.cs
@@ -33,6 +33,8 @@ namespace Services.Commons.Gmail
 
         public async Task SendEmailAsync(List<string> to, string subject, string message)
         {
+            await Task.Delay(1000); // ~0.6s để đảm bảo không quá 2 request/s
+
             // Validate inputs
             if (to == null || to.Count == 0)
                 throw new ArgumentException("Recipient list cannot be empty", nameof(to));
