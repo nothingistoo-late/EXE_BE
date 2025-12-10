c690ae2a713e79639013ab7eb44b80799c29f18d
hctrung2k4 <hctrung2k4@gmail.com>
2025-12-10 13:09:21 +0700

    Update CORS

--- WebAPI/Extensions/InfrastructureServiceCollectionExtensions.cs
+++ WebAPI/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -37,7 +37,7 @@ namespace WebAPI.Extensions
             services.AddCors(opt =>
             {
                 opt.AddPolicy("CorsPolicy", b => b
-                    .WithOrigins("https://www.vegie.site", "https://vegie.site", "http://localhost:5173", "http://localhost:3000", "https://presentation-ashy-seven.vercel.app")
+                    .WithOrigins("https://www.vegie.site", "https://vegie.site", "http://localhost:5173", "http://localhost:3000", "https://vnr-202-rho.vercel.app")
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()
